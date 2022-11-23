9026b41467887842d056d48a58b4e6b0ce392e54
MauVarva14 <118914100+MauVarva14@users.noreply.github.com>
2022-11-23 09:08:44 -0600

    Todo en orden parece

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector(".toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+
+    if (navMenu.classList.contains("nav-menu_visible")) {
+        navToggle.setAttribute("arial-label", "Close menu");
+    } else {
+        navToggle.setAttribute("arial-label", "Open menu");
+    }
+
+});
